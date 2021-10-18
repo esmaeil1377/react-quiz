1c2ba95a96430fad7289f3a3b6f27b8fc11d106c
esmaeil1377 <pahang1378@gmail.com>
2021-10-18 15:19:35 +0330

    add images and declare state we need

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GlobalStyle, Wrapper } from "./App.styles";
+import { Form, Row, Col, Button } from "react-bootstrap";
 
-function App() {
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  const [startB, setStartB] = useState(true);
+  // const [categori, setCategori] = useState(["Any Category", ])
+  const [qNumber, setQNumber] = useState("");
+  const [qCategory, setQCategory] = useState("");
+  const [qDifficulty, setQDifficulty] = useState("");
+  const [qType, setQType] = useState("");
   return (
     <>
-      
+      <GlobalStyle />
+      <Wrapper>
+      <h1>REACT QUIZ</h1>
+
+      </Wrapper>
     </>
   );
 }
