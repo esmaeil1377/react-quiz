b2d6ab1885721bf5214f8c5fd82a2d12f7898b70
sh3rLock3d <alipourghasemi78@gmail.com>
2021-10-18 18:52:30 +0330

    add prop to QuestionCard

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,12 +1,33 @@
 import React from "react";
+// Types
+import { AnswerObject } from "../App";
+// Styles
 
 
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
 
-const QuestionCard = () => {
-    return <div>
-        im question cart
-    </div>
-}
-
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    
+  </>
+);
 
 export default QuestionCard;
